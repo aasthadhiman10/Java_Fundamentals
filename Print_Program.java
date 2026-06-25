15fca801beceab364bc52da13c4e08a929a631bb
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-25 17:54:57 +0530

    Add initial Java print program

---
+++ Print_Program.java
@@ -0,0 +1,6 @@
+public class Print_Program {
+    public static void main(String[]args){
+        System.out.println("This is my first java program");
+        System.out.print("Hello World");
+    }
+}
