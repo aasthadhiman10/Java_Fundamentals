62f9aaded0a5ea24410cebb37b01915c021295a7
Aastha Dhiman <aasthadhimann@gmail.com>
2026-06-25 18:18:09 +0530

    Add basic input/output Java program

---
+++ Input_Output_Program.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Input_Output_Program {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Name : ");
+        String name = sc.nextLine();
+            System.out.print("Your Name is : " + name );
+
+    }    
+}
